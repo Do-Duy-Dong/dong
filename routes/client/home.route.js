7f0d0345416d2db24edd980cc83ab2141fabd26f
Ten <email@example.com>
2024-10-01 15:56:33 +0700

    fix

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -4,5 +4,5 @@ const controller=require("../../controller/client/home.controller.js")
 module.exports = homeRouters;
 
 homeRouters.get("/", controller.index);
-
+// asd
 homeRouters.get("/home",controller.index);
